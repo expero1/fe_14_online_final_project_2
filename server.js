449d7ff0aca3204ebc69fc6798e3f54fc222f362
Eugene Emets <eu.emets@gmail.com>
2023-08-08 18:39:48 +0300

    add gzip static files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressGzip = require('express-static-gzip');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -72,8 +73,10 @@ app.use('/api/partners', partners);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static(path.join(__dirname, 'static')));
+  //   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(expressGzip(path.join(__dirname, 'client/build')));
+  //   app.use(express.static(path.join(__dirname, 'static')));
+  app.use(expressGzip(path.join(__dirname, 'static')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
